db3a1c8979f0015341168700819da7174558a76c
Antony Donovan <antony.donovan@generalassemb.ly>
2016-05-12 13:26:59 -0400

    Read file/get type
    Use file-type to determine content-type
    for s3 upload

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
+
+const mimeType = (data) =>
+  Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream',
+  }, fileType(data));
 
 let filename = process.argv[2] || '';
 
@@ -15,7 +22,24 @@ const readFile = (filename) =>
     });
   });
 
+const awsUpload = (file) => {
+  const options = {
+    ACL: 'public-read',
+    Body: file.data,
+    Bucket: 'gaand-011',
+    ContentType: file.mime,
+    Key: `test/test.${file.ext}`,
+  };
+  return Promise.resolve(options);
+};
+
 readFile(filename)
-.then((data) =>
-  console.log(`${filename} is ${data.length} bytes long`))
+.then((data) => {
+  let file = mimeType(data);
+  file.data = data;
+  return file;
+})
+.then(awsUpload)
+.then((options) =>
+  console.log(options))
 .catch(console.error);
